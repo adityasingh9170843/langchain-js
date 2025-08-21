e7677ff15eec6205cbeb159199822079cbc62871
Aditya Singh <adityaa9170843@gmail.com>
2025-08-21 21:46:07 +0530

    feat: refactor main function to streamline web search integration and improve response handling

--- llm.js
+++ llm.js
@@ -3,7 +3,6 @@ process.on("warning", () => {});
 import { GoogleGenAI } from "@google/genai";
 import dotenv from "dotenv";
 import { tavily } from "@tavily/core";
-import { json, text } from "stream/consumers";
 dotenv.config({ quiet: true });
 
 const tvly = tavily({ apiKey: process.env.TAVILY_API_KEY });
@@ -31,71 +30,61 @@ const webSearchTool = {
 async function main() {
   const systemInstruction = `You are a smart personal assistant who answers the asked questions.
         you have access to following tools :
-        1.webSearch((query):{query:"String"}) //Search the latest information and realtime news about a topic.`
+        1.webSearch((query):{query:"String"}) //Search the latest information and realtime news about a topic.`;
 
-  const userPrompt = {
-    role:"user",
-    parts:[{text:"What's the current price of Nvidia stock?"}]
-  }
-  const response = await ai.models.generateContent({
-    model: "gemini-2.5-flash",
-    contents: userPrompt,
-    config: {
-      tools: [webSearchTool],
-      systemInstruction: systemInstruction,
+  const contents = [
+    {
+      role: "user",
+      parts: [{ text: "What's the current price of Nvidia stock?" }],
     },
-  });
+  ];
 
+  while (true) {
+    const response = await ai.models.generateContent({
+      model: "gemini-2.5-flash",
+      contents: contents,
+      config: {
+        tools: [webSearchTool],
+        systemInstruction: systemInstruction,
+      },
+    });
 
+    const toolCalls = response.functionCalls;
 
-  const toolCalls = response.functionCalls;
-
-  if (!toolCalls) {
-    console.log("No tool calls found");
-    return;
-  }
-
-  let result;
-  for (const tool of toolCalls) {
-    const functionName = tool.name;
-    const params = tool.args;
-
-    if (functionName === "webSearch") {
-      result = await webSearch(params);
-      
+    if (!toolCalls) {
+      console.log(response.text);
+      break
     }
-  }
 
-  const finalResponse = await ai.models.generateContent({
-    model: "gemini-2.5-flash",
-    contents:[
-      userPrompt,
-      response.candidates[0].content,
-      {
-        role:"tool",
-        parts:[{
-          functionResponse:{
-            name:"webSearch",
-            response:{
-              content:result
-            }
-          }
-        }]
+    let result;
+    for (const tool of toolCalls) {
+      const functionName = tool.name;
+      const params = tool.args;
+
+      if (functionName === "webSearch") {
+        result = await webSearch(params);
       }
-    ],
-    config: {
-      tools: [webSearchTool],
-      systemInstruction: systemInstruction,
-    },
-  })
+    }
 
-  console.log(finalResponse.text)
+    contents.push({
+      role: "tool",
+      parts: [
+        {
+          functionResponse: {
+            name: "webSearch",
+            response: { content: result },
+          },
+        },
+      ],
+    });
+  }
 }
 
 main();
 
 async function webSearch({ query }) {
   const response = await tvly.search(query);
+
   const result = response.results.map((result) => result.content).join("\n\n");
 
   return result;
